cd2876d154a55fda9b40a09eff5b3d190ef3e2a8
wegenbabi73-blip <wegenbabi73@gmail.com>
2026-09-22 20:49:55 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,8 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'et.gov.customs.ecc.tips',
+  appName: 'ECC Tip System',
+  webDir: 'www',
+  server: { androidScheme: 'https' }
+};
+export default config;
